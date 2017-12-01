ead0e977314c3982e7348849013fb2a595954140
itsHabib <michaelhabib1868@gmail.com>
2017-11-30 16:43:49 -0800

    finalize demo

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/url"
 	"os"
-	"strconv"
 
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/aws/aws-sdk-go/aws"
@@ -19,35 +19,38 @@ func main() {
 	awsSession := session.New(&aws.Config{Region: &region})
 	streamName := "541-Demo-Stream"
 
-	// Authenticate Twitter
-	// var twitterData []string
+	// Twitter Credentials
 	TWITTER_ACCESS_TOKEN := os.Getenv("TWITTER_ACCESS_TOKEN")
 	TWITTER_TOKEN_SECRET := os.Getenv("TWITTER_TOKEN_SECRET")
 	TWITTER_CONSUMER_KEY := os.Getenv("TWITTER_CONSUMER_KEY")
 	TWITTER_CONSUMER_SECRET := os.Getenv("TWITTER_CONSUMER_SECRET")
+
+	// Authenticate Twitter
 	anaconda.SetConsumerKey(TWITTER_CONSUMER_KEY)
 	anaconda.SetConsumerSecret(TWITTER_CONSUMER_SECRET)
 	twitterAPI := anaconda.NewTwitterApi(TWITTER_ACCESS_TOKEN, TWITTER_TOKEN_SECRET)
 
+	// Kinesis Client used for Putting records, reading records, etc..
 	kinesisClient := kinesis.New(awsSession)
 
 	v := url.Values{}
 	twitterStreamHandler := twitterAPI.PublicStreamSample(v)
 	statusCount := 0
 	for statusCount < 10 {
+		// tweet data comes in through a channel
 		item := <-twitterStreamHandler.C
 		switch status := item.(type) {
 		case anaconda.Tweet:
 			partitionKey := fmt.Sprintf("partitionKey-%d", statusCount)
 			_, err := kinesisClient.PutRecord(&kinesis.PutRecordInput{
-				Data:         []byte(strconv.Itoa(status.RetweetCount)),
+				Data:         []byte(status.Text),
 				StreamName:   &streamName,
 				PartitionKey: aws.String(partitionKey),
 			})
 			if err != nil {
 				panic(err)
 			}
-
+			log.Println(status.Text)
 			statusCount++
 		}
 	}
